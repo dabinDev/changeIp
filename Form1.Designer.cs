ec08f5d92fee25ddbeb26e1830c10cb2bd7a3c2b
dabin <cage_ben@sina.com>
2019-08-20 11:49:31 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.input_name = new System.Windows.Forms.TextBox();
+            this.input_password = new System.Windows.Forms.TextBox();
+            this.input_address = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "链接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // input_name
+            // 
+            this.input_name.Location = new System.Drawing.Point(155, 88);
+            this.input_name.Name = "input_name";
+            this.input_name.Size = new System.Drawing.Size(189, 25);
+            this.input_name.TabIndex = 1;
+            // 
+            // input_password
+            // 
+            this.input_password.Location = new System.Drawing.Point(155, 184);
+            this.input_password.Name = "input_password";
+            this.input_password.Size = new System.Drawing.Size(189, 25);
+            this.input_password.TabIndex = 2;
+            // 
+            // input_address
+            // 
+            this.input_address.Location = new System.Drawing.Point(155, 136);
+            this.input_address.Name = "input_address";
+            this.input_address.Size = new System.Drawing.Size(189, 25);
+            this.input_address.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "链接名称";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "链接密码";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "链接地址";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(247, 365);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "断开";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 423);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.input_address);
+            this.Controls.Add(this.input_password);
+            this.Controls.Add(this.input_name);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox input_name;
+        private System.Windows.Forms.TextBox input_password;
+        private System.Windows.Forms.TextBox input_address;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Base;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    private void Button1_Click(object sender, EventArgs e)
+        {
+            connect();
+        }
+
+        private void connect()
+        {
+            String name = input_name.Text;
+            String adddress = input_address.Text;
+            String password = input_password.Text;
+            String message = "测试链接";
+            if (name==null||name.Equals("")) {
+                MessageBox.Show("宽带名称不能为空，通常为'默认宽带'");
+                return;
+            }
+            if (adddress == null || adddress.Equals(""))
+            {
+                MessageBox.Show("链接地址不能为空！");
+                return;
+            }
+            if (password == null || password.Equals(""))
+            {
+                MessageBox.Show("链接密码不能为空！");
+                return;
+            }
+            Adsl.Connect(name, adddress, password, ref message);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Adsl.Disconnect();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //注册热键Shift+S，Id号为100。HotKey.KeyModifiers.Ctrl也可以直接使用数字4来表示。   
+            HotKey.RegisterHotKey(Handle, 100, HotKey.KeyModifiers.Ctrl, Keys.F11);
+            //注册热键Ctrl+B，Id号为101。HotKey.KeyModifiers.Ctrl也可以直接使用数字2来表示。   
+            HotKey.RegisterHotKey(Handle, 101, HotKey.KeyModifiers.Ctrl, Keys.F12);
+        }
+
+        
+        protected override void WndProc(ref Message m)   
+        {   
+            const int WM_HOTKEY = 0x0312;   
+            //按快捷键    
+            switch (m.Msg)   
+            {   
+                case WM_HOTKEY:   
+                    switch (m.WParam.ToInt32())   
+                    {   
+                        case 100:    //按下的是Shift+S   
+                            connect();
+                            break;   
+                        case 101:    //按下的是Ctrl+B   
+                            //此处填写快捷键响应代码   
+                            MessageBox.Show("断开链接");
+            Adsl.Disconnect();
+                            break;     
+                    }   
+                    break;   
+            }   
+            base.WndProc(ref m);   
+        } 
+    }
+}
